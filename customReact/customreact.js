05969bcb9de105f6d5dde45b19a29b3c89504456
007Edge <edge07.shiv@gmail.com>
2025-07-06 02:21:17 +0530

    version 2 (added a loop for setting the attributes)

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,8 +1,24 @@
+// version 1
+
+/*
 function customRender(reactElement, container) {
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
     domElement.setAttribute('href', reactElement.props.href);
     domElement.setAttribute('target', reactElement.props.target);
+    container.appendChild(domElement);
+}
+*/
+
+// version 2
+
+function customRender(reactElement, container) {
+    const domElement = document.createElement(reactElement.type);
+    domElement.innerHTML = reactElement.children;
+
+    for (const prop in reactElement.props) {
+        domElement.setAttribute(prop, reactElement.props[prop]); s // prop is key and (reactElement.props[prop]) is value of key
+    }
     container.appendChild(domElement);
 }
 
